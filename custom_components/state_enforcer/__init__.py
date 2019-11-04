19a9c1767fb11475bf0c86a5fdd581efa6fd1687
Mikhail Boldyrev <miboldyrev@gmail.com>
2019-11-04 13:32:06 +0300

    state_enforcer: added set_light and some fixes

--- custom_components/state_enforcer/__init__.py
+++ custom_components/state_enforcer/__init__.py
@@ -1,4 +1,4 @@
-"""The complex_controller integration."""
+"""The state_enforcer integration."""
 import asyncio
 import logging
 import voluptuous as vol
@@ -7,7 +7,7 @@ import homeassistant.helpers.config_validation as cv
 from homeassistant.setup import async_setup_component
 from homeassistant.const import (ATTR_ENTITY_ID, ATTR_SERVICE,
                                  ATTR_SERVICE_DATA, ATTR_STATE, CONF_ENTITY_ID,
-                                 EVENT_STATE_CHANGED)
+                                 EVENT_STATE_CHANGED, STATE_ON, STATE_OFF)
 
 from .const import *
 
@@ -16,6 +16,8 @@ SLEEP_MULTIPLIER = 1.5
 SLEEP_MAX = 180
 
 CONFIG_SCHEMA = vol.Schema({DOMAIN: [cv.entity_id]}, extra=vol.ALLOW_EXTRA)
+
+LIGHT_SERVICES = {STATE_ON: 'light.turn_on', STATE_OFF: 'light.turn_off'}
 
 SERVICE_SET_STATE_SCHEMA = vol.Schema(
     {
@@ -28,37 +30,73 @@ SERVICE_SET_STATE_SCHEMA = vol.Schema(
     extra=vol.ALLOW_EXTRA,
     required=True)
 
+SERVICE_SET_LIGHT_SCHEMA = vol.Schema(
+    {
+        ATTR_ENTITY_ID: cv.entity_id,
+        ATTR_STATE: vol.In(LIGHT_SERVICES),
+        vol.Optional(ATTR_BRIGHTNESS): int
+    },
+    extra=vol.ALLOW_EXTRA,
+    required=True)
+
 state_enforcers = dict()
 
 _LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup(hass, config):
-    """Set up the complex_controller integration."""
+    """Set up the state_enforcer integration."""
     # try/catch?
     for entity_id in config[DOMAIN]:
         state_enforcers[entity_id] = await StateEnforcer.create(
             hass, entity_id)
 
-    hass.services.async_register(DOMAIN,
-                                 SERVICE_SET_STATE,
-                                 async_set_state,
-                                 schema=SERVICE_SET_STATE_SCHEMA)
+    register_service_handler(hass, SERVICE_SET_STATE, async_set_state,
+                             SERVICE_SET_STATE_SCHEMA)
+    register_service_handler(hass, SERVICE_SET_LIGHT, async_set_light,
+                             SERVICE_SET_LIGHT_SCHEMA)
 
     hass.bus.async_listen(EVENT_STATE_CHANGED, async_on_state_changed)
 
     return True
 
 
-async def async_set_state(event):
-    entity_id = event.data[ATTR_ENTITY_ID]
-    state_enforcer = state_enforcers.get(entity_id)
-    if state_enforcer is None:
-        _LOGGER.error(
-            f'Got service {SERVICE_SET_STATE} call with {ATTR_ENTITY_ID} = {entity_id} which is not set up!'
-        )
-        return
-    await state_enforcer.handle_enforce_state(event)
+def register_service_handler(hass, service_name, handler, schema):
+    async def service_handler_wrapper(event):
+        entity_id = event.data[ATTR_ENTITY_ID]
+        state_enforcer = state_enforcers.get(entity_id)
+        if state_enforcer is None:
+            _LOGGER.error(f'Got service {service_name} call '
+                          f'with {ATTR_ENTITY_ID} = {entity_id} '
+                          'which is not set up!')
+            return
+        await handler(state_enforcer, event)
+
+    hass.services.async_register(DOMAIN,
+                                 service_name,
+                                 service_handler_wrapper,
+                                 schema=schema)
+
+
+async def async_set_state(state_enforcer, event):
+    await state_enforcer.handle_enforce_state(
+        service=event.data[ATTR_SERVICE],
+        service_data=event.data.get(ATTR_SERVICE_DATA, dict()),
+        state=event.data[ATTR_STATE],
+        state_attrs=event.data.get(ATTR_STATE_ATTRIBUTES, dict()))
+
+
+async def async_set_light(state_enforcer, event):
+    on_off = event.data[ATTR_STATE]
+    state_attrs = dict()
+    if event.data.get(ATTR_BRIGHTNESS) is not None:
+        state_attrs[ATTR_BRIGHTNESS] = event.data.get(ATTR_BRIGHTNESS)
+    service_data = state_attrs.copy()
+    service_data[ATTR_ENTITY_ID] = event.data[ATTR_ENTITY_ID]
+    await state_enforcer.handle_enforce_state(service=LIGHT_SERVICES[on_off],
+                                              service_data=service_data,
+                                              state=on_off,
+                                              state_attrs=state_attrs)
 
 
 async def async_on_state_changed(event):
@@ -85,11 +123,11 @@ class StateEnforcer(object):
     async def async_on_state_changed(self, event):
         await self.check_current_state()
 
-    async def handle_enforce_state(self, event):
-        self.service = SplitId(event.data[ATTR_SERVICE])
-        self.service_data = event.data.get(ATTR_SERVICE_DATA, dict())
-        self.state = event.data[ATTR_STATE]
-        self.state_attrs = event.data.get(ATTR_STATE_ATTRIBUTES, dict())
+    async def handle_enforce_state(self, service, service_data, state, state_attrs):
+        self.service = SplitId(service)
+        self.service_data = service_data
+        self.state = state
+        self.state_attrs = state_attrs
         self.retry_number = 0
         await self.enforce()
 
@@ -100,6 +138,7 @@ class StateEnforcer(object):
         if current_state.state != self.state or any(
                 current_state.attributes.get(k) != v
                 for k, v in self.state_attrs.items()):
+            self.retry_number += 1
             await self.enforce()
 
     async def enforce(self):
--- custom_components/state_enforcer/const.py
+++ custom_components/state_enforcer/const.py
@@ -1,7 +1,9 @@
-"""Constants for the complex_controller integration."""
+"""Constants for the state_enforcer integration."""
 
 DOMAIN = 'state_enforcer'
 
 SERVICE_SET_STATE = 'set_state'
+SERVICE_SET_LIGHT = 'set_light'
 
 ATTR_STATE_ATTRIBUTES = 'state_attributes'
+ATTR_BRIGHTNESS = 'brightness'
